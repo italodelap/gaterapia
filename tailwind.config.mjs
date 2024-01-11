e6dea936a8fce71c46e3d8ab49c2ab78646ab987
Italo De la Peña <italo.delapena@utdt.edu>
2024-01-11 16:31:42 -0300

    feat(*): 💄 add Gaterapia's colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import defaultTheme from "tailwindcss/defaultTheme"
 import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
@@ -8,6 +9,21 @@ export default {
 	],
 	theme: {
 		extend: {
+			colors: {
+				'cerise-red': {
+					'50': '#fdf2f7',
+					'100': '#fce7f1',
+					'200': '#fccee3',
+					'300': '#fba6cb',
+					'400': '#f76fa8',
+					'500': '#f04487',
+					'600': '#e1306c',
+					'700': '#c2144a',
+					'800': '#a0143d',
+					'900': '#851637',
+					'950': '#52051c',
+				},
+			},
 			sans: ["Caveat Brush", ...defaultTheme.fontFamily.sans]
 		},
 	},
