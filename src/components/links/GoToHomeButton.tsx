1538e2620a7f51dbb72975495f5d09d39aa24336
Italo De la Peña <italo.delapena@utdt.edu>
2024-01-28 00:25:42 -0300

    feat(/links): ✨ create links page

--- src/components/links/GoToHomeButton.tsx
+++ src/components/links/GoToHomeButton.tsx
@@ -1,17 +1,12 @@
 import { Button, Link } from "@nextui-org/react";
 
-import { LinkIcon } from "./LinkIcon";
-
-export function LinksButton({ className }: { className?: string }) {
+export function GoToHomeButton({ className }: { className?: string }) {
 	return (
 		<Button
+			href="/"
 			as={Link}
-			size="md"
-			radius="lg"
-			href="/links"
+			radius="sm"
 			variant="ghost"
-			color="primary"
-			startContent={<LinkIcon className="size-5" />}
 			className={`
 				${className ?? ""}
         border-white text-white
@@ -19,7 +14,7 @@ export function LinksButton({ className }: { className?: string }) {
         hover:border-cerise-red-600 hover:!bg-cerise-red-600
 			`}
 		>
-			Links
+			Ir a Gaterapia
 		</Button>
 	);
 }
