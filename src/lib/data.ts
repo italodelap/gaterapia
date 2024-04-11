007625eb9a6e5acde5726928446d72fe2a24ea09
Italo De la Peña <italo.delapena@utdt.edu>
2024-04-10 22:49:36 -0300

    feat(services-content): ✨ automate services creation based on content

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,17 +1,27 @@
-const SERVICES = [
-	"Catsitting",
-	"Terapia Felina",
-	"Terapia Floral",
-	"Reiki",
-	"Registros Akáshicos",
-	"Comunicación telepática con animales",
-];
+import { getCollection } from "astro:content";
 
-export function getCompanyServices(): string {
+import type { FormattedService } from "./types";
+
+export function getServicesDescriptionForSeo(): string {
+	const SERVICES = [
+		"Catsitting",
+		"Terapia Felina",
+		"Terapia Floral",
+		"Reiki",
+		"Registros Akáshicos",
+		"Comunicación telepática con animales",
+	];
+
 	let phrase = "";
 	SERVICES.forEach((service, index) => {
 		phrase += index < SERVICES.length - 1 ? `${service}, ` : `y ${service}`;
 	});
 
 	return phrase;
+}
+
+export async function getFormattedServices(): Promise<FormattedService[]> {
+	const services = await getCollection("services");
+
+	return services.map(({ slug, data }) => ({ slug, ...data }));
 }
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,3 +1,8 @@
+import type { CollectionEntry } from "astro:content";
+
+type ServiceEntry = CollectionEntry<"services">;
+export type FormattedService = ServiceEntry["data"] & { slug: ServiceEntry["slug"] };
+
 export interface Link {
 	link: string;
 	url: string;
