99f5430a7d9d421108150b7ed10e2e70bd535cb7
Italo De la Peña <italo.delapena@utdt.edu>
2024-04-15 17:31:41 -0300

    feat(*): 📈 add Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), react()],
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 });
