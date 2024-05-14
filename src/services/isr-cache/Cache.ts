15a92453dd1fbcd8cd5663f4ba32bb08d83d126f
Italo De la Peña <italo.delapena@utdt.edu>
2024-05-14 09:58:53 -0300

    revert(*): ⏪️ revert ISR implementation

--- src/services/isr-cache/Cache.ts
+++
@@ -1,8 +0,0 @@
-export interface Cache<P> {
-	// Get a value from the cache.
-	get(key: string): P | undefined;
-	// Set a value in the cache.
-	set(key: string, value: P): void;
-	// Delete a value from the cache.
-	del(key: string): void;
-}
--- src/services/isr.ts
+++
@@ -1,9 +0,0 @@
-import { IsrService } from "./isr-cache/IsrService";
-import { MemoryCache } from "./isr-cache/MemoryCache";
-
-const cache = new MemoryCache<Response>();
-export const isr = new IsrService(cache);
-
-export function invalidate(key: string): void {
-	isr.del(key);
-}
